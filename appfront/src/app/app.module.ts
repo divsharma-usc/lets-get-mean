da141a2c6589050f9a4a97627bb3a838387a14ad
Divyansh Sharma <divyansh9595@gmail.com>
2017-08-21 23:44:26 +0530

    auth added

--- appfront/src/app/app.module.ts
+++ appfront/src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { AuthenticationService } from './services/authentication.service';
+import { AuthGuard } from './guards/auth.guard';
+
+const appRoutes: Routes=[
+   { path : '', component : HomeComponent, canActivate:[AuthGuard]},
+   { path: 'login', component : LoginComponent},
+   { path : 'signup', component: RegisterComponent},
+   { path : ' **',redirectTo: ''}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true }
+    ),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
   ],
   providers: [AuthenticationService],
   bootstrap: [AppComponent]
--- appfront/src/app/login/login.component.ts
+++ appfront/src/app/login/login.component.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import {User} from '../models/user';
+import {AuthenticationService} from '../services/authentication.service';
+
+@Component({
+  templateUrl: './login.component.html'
+})
+
 export class LoginComponent{
-  
+
 }
