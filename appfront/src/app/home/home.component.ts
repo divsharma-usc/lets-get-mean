dec18e7fb7544614e3fa1f5be9ce62098c358815
Divyansh Sharma <divyansh9595@gmail.com>
2017-09-02 01:09:21 +0530

    slider added

--- appfront/src/app/home/home.component.ts
+++ appfront/src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 
 import { User } from '../models/user';
 import { Course } from '../models/course';
+import {Observable} from 'rxjs/Rx';
 
 
 @Component({
@@ -18,12 +19,20 @@ export class HomeComponent implements OnInit{
    seta2:any;
    seta3:any;
    images=['a.jpg','b.jpg','c.jpg','d.jpg','e.jpg','f.jpg']
+   i:number;
+   quote:string;
+   author:string;
+   authors=["Steve Jobes","Bill Gates","Salvador Dali","Donatella Versace"]
+   quotes=["I think everybody in this country should learn to program a computer. Learn a computer language. Because it teaches you how to think.","I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.","Have no fear of perfection, you’ll never reach it","Creativity comes from a conflict of ideas"];
    constructor(private http:Http,
    private router:Router){
       this.setcolor=["#30bce1","#22c2b1","#b068e6","#6a7c8e","#fa6d7d","#ed9c43"];
       this.seta1=[2,2,169,125,255,255];
       this.seta2=[179,204,181,151,162,84];
       this.seta3=[228,168,237,173,52,131];
+      this.i=0;
+      this.quote=this.quotes[0]
+      this.author=this.authors[0];
    }
    ngOnInit():void{
        this.http.get('http://localhost:3000/home').subscribe(data=>{
@@ -33,7 +42,10 @@ export class HomeComponent implements OnInit{
          }
          this.checkCourse();
        })
-
+       let timer = Observable.timer(5000,5000);
+        timer.subscribe(t=> {
+        this.next();
+      });
 
    }
    enroll(course):void{
@@ -65,9 +77,28 @@ export class HomeComponent implements OnInit{
 
    }
   getcolors(i):any{
-     var b={background:'linear-gradient(rgba('+this.seta1[i]+','+this.seta2[i]+','+this.seta3[i]+','+'.8),rgba('+this.seta1[i]+','+this.seta2[i]+','+this.seta3[i]+','+'.8)),url(/assets/images/'+this.images[i]+')'}
-     console.log(b);
+     var b={background:'linear-gradient(rgba('+this.seta1[i]+','+this.seta2[i]+','+this.seta3[i]+','+'.8),rgba('+this.seta1[i]+','+this.seta2[i]+','+this.seta3[i]+','+'.8)),url(/assets/images/'+this.images[i]+')'};
      return b;
+  }
+  next(){
+     if(this.i<3){
+         this.i=this.i+1;
+     }
+     else{
+         this.i=0;
+     }
+     this.quote=this.quotes[this.i];
+     this.author=this.authors[this.i];
+  }
+  previous(){
+     if(this.i>0){
+         this.i=this.i-1;
+     }
+     else{
+         this.i=3;
+     }
+      this.quote=this.quotes[this.i];
+      this.author=this.authors[this.i];
   }
 
  }
